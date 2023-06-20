1676bc5783b922af44b6476f0eb8cc7ab6529ba5
Ars3ym0s <arseymos.elkin@bk.ru>
2023-06-20 23:21:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const double g = 9.81;
+            double h, v, t;
+            Console.Write("Введите высоту (м): ");
+
+                h = double.Parse(Console.ReadLine());
+                v = Math.Sqrt(2 * g * h);
+                t = Math.Sqrt(2 * h / g);
+
+            Console.WriteLine("");
+            Console.WriteLine("Скорость падения: {0:F1} м/с", v);
+            Console.WriteLine("Время падения: {0:F2} сек", t);
+        }
+    }
+}
